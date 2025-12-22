b87632656921c8c165f052d1579b5b96f7de11b7
fauzsanno <128324591+fauzsanno@users.noreply.github.com>
2025-12-22 10:10:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.markdown(
     """
     <style>
     .stApp {
-            background-color: #8CC2F1;
+            background-color: #135792;
     }
     </style>
     """,
@@ -137,6 +137,7 @@ st.caption(
     "- Kolesterol total normal: <200 mg/dL\n"
     "Aplikasi ini bersifat pendukung keputusan dan tidak menggantikan diagnosis medis."
 )
+
 
 
 
